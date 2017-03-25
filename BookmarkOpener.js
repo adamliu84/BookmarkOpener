d1e983f58f00ac23570b192bfa817c2ebf9b9831
adamliu84 <adam.liu84@gmail.com>
2017-03-25 08:59:00 -0700

    Reading urls listing from text fie

--- BookmarkOpener.js
+++ BookmarkOpener.js
@@ -5,7 +5,21 @@ var openPage = function(szUrl){
     });
 }
 
+function readDataFile(){
+  //Ref: http://stackoverflow.com/questions/13832251/how-to-read-and-display-file-in-a-chrome-extension
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', chrome.extension.getURL('data.txt'), true);
+  xhr.onreadystatechange = function()
+  {
+      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+      {
+        var obj = JSON.parse(xhr.responseText);
+        obj.urls.map(openPage);
+      }
+  };
+  xhr.send();
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  pages = ['https://www.yahoo.com','https://www.google.com','https://duckduckgo.com/','https://www.bing.com']
-  pages.map(openPage)
+  readDataFile();
 });
